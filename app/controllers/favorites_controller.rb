ef2b166ffa8d5d1ce176b604886c14aeed459692
shiho-h <sisopiku@gmail.com>
2020-08-25 16:07:08 +0900

    favorite_Ajex finish

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,14 @@ class FavoritesController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		cat_image = CatImage.find(params[:cat_image_id])
-		favorite = current_user.favorites.new(cat_image_id: cat_image.id)
+		@cat_image = CatImage.find(params[:cat_image_id])
+		favorite = current_user.favorites.new(cat_image_id: @cat_image.id)
 		favorite.save
-		redirect_to cat_image_path(cat_image)
 	end
 
 	def destroy
-    	cat_image = CatImage.find(params[:cat_image_id])
-    	favorite = current_user.favorites.find_by(cat_image_id: cat_image.id)
+    	@cat_image = CatImage.find(params[:cat_image_id])
+    	favorite = current_user.favorites.find_by(cat_image_id: @cat_image.id)
     	favorite.destroy
-    	redirect_to cat_image_path(cat_image)
 	end
 end
